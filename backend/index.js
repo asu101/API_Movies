187af9ddfa524a6ef5f214eb629079434ddafe25
asu <asu101ale@gmail.com>
2023-01-24 17:10:09 +0100

    Initial commit

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const app = express();
+const { mongoose } = require('./database');
+const {json} = require("express");
+
+// Middleware
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use('/api/movies', require('./routes/movie.route'));
+app.use('/', (req, res) => res.send('API in /api/movies'));
+
+// Starting the server
+app.listen(app.get('port'), () => {
+    console.log('Server on port: ', app.get('port'));
+});
